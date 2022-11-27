a1908ba4f7addd29b22a5090f89d4f1d3cc34acb
Fábio Mendes <fabiobends@gmail.com>
2022-11-27 19:14:29 -0300

    feat: add initial logic to button
    "Selected Date" button display a toast with some text related to
    `DatePickerDialog`

--- app/src/main/java/com/example/dobcalc/MainActivity.kt
+++ app/src/main/java/com/example/dobcalc/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.example.dobcalc
 
+import android.app.DatePickerDialog
+import android.icu.util.Calendar
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    val btnDatePicker: Button = findViewById(R.id.btnDatePicker)
+    btnDatePicker.setOnClickListener {
+      clickDatePicker()
+    }
+  }
+
+  fun clickDatePicker() {
+    val myCalendar = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+      Calendar.getInstance()
+    } else {
+      TODO("VERSION.SDK_INT < N")
+    }
+    val year = myCalendar.get(Calendar.YEAR)
+    val month = myCalendar.get(Calendar.MONTH)
+    val day = myCalendar.get(Calendar.DAY_OF_MONTH)
+    DatePickerDialog(this, { view, year, month, day ->
+      Toast.makeText(this, "btnDatePicked pressed ${year + 1} + ${month - 1}", Toast.LENGTH_LONG)
+        .show()
+    }, year, month, day).show()
   }
 }
