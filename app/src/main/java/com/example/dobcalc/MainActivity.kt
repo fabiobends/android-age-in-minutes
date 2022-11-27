8a16f93c1d9ed5739a05c1fd549594ab57de6e4d
Fábio Mendes <fabiobends@gmail.com>
2022-11-27 19:59:32 -0300

    feat: add logic to display difference in minutes
    - Manipulate the date parsing it, using the propety `time` afterwards
    - Present the text in both `tvSelectedDate` and `tvDateDifference` ids

--- app/src/main/java/com/example/dobcalc/MainActivity.kt
+++ app/src/main/java/com/example/dobcalc/MainActivity.kt
@@ -6,32 +6,57 @@ import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.annotation.RequiresApi
+import java.text.SimpleDateFormat
+import java.util.*
+
+const val MILLISECONDS_MINUTES_RATIO = 60 * 1000;
+const val MAX_TIME = 86400000;
 
 class MainActivity : AppCompatActivity() {
+
+  private var tvSelectedDate: TextView? = null
+  private var tvDateDifference: TextView? = null
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
 
     val btnDatePicker: Button = findViewById(R.id.btnDatePicker)
+    tvSelectedDate = findViewById(R.id.tvSelectedDate)
+    tvDateDifference = findViewById(R.id.tvDateDifference)
     btnDatePicker.setOnClickListener {
       clickDatePicker()
     }
   }
 
-  fun clickDatePicker() {
+  private fun clickDatePicker() {
     val myCalendar = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
       Calendar.getInstance()
     } else {
-      TODO("VERSION.SDK_INT < N")
+      throw Error("SDK Version is old")
     }
     val year = myCalendar.get(Calendar.YEAR)
     val month = myCalendar.get(Calendar.MONTH)
     val day = myCalendar.get(Calendar.DAY_OF_MONTH)
-    DatePickerDialog(this, { view, year, month, day ->
-      Toast.makeText(this, "btnDatePicked pressed ${year + 1} + ${month - 1}", Toast.LENGTH_LONG)
-        .show()
-    }, year, month, day).show()
+    val datePickerDialog = DatePickerDialog(this, { _, selectedYear, selectedMonth, selectedDay ->
+      val selectedDate = "$selectedDay/${selectedMonth + 1}/$selectedYear"
+      tvSelectedDate?.text = selectedDate
+      val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+      val date = dateFormat.parse(selectedDate)
+      date?.let {
+        val selectedDateInMinutes = date.time / MILLISECONDS_MINUTES_RATIO
+        val currentDate = dateFormat.parse(dateFormat.format(System.currentTimeMillis()))
+        currentDate?.let {
+          val currentDateInMinutes = currentDate.time / MILLISECONDS_MINUTES_RATIO
+          val difference = currentDateInMinutes - selectedDateInMinutes
+          tvDateDifference?.text = difference.toString()
+        }
+      }
+    }, year, month, day)
+    datePickerDialog.datePicker.maxDate = System.currentTimeMillis() - MAX_TIME
+    datePickerDialog.show()
   }
 }
